663b81f2745fd067538f81829a4a6db992731b85
Michalis Koutridis <michalis.koutridis@camelotls.com>
2020-05-23 14:25:19 +0300

    improvement(styles): improves student ui

--- src/app/Views/StudentView.js
+++ src/app/Views/StudentView.js
@@ -4,7 +4,7 @@ import {
 	constructElement,
 	studentElement,
 	clearInnerContent,
-	studentComments
+	studentComments,
 } from '../../utils/utilities';
 import isEmpty from 'lodash/isEmpty';
 import { STUDENT_CARD } from '../../utils/config';
@@ -18,7 +18,7 @@ class StudentView {
 	}
 
 	bindCommentInput(handler) {
-		getElementDOM('#root').addEventListener('click', event => {
+		getElementDOM('#root').addEventListener('click', (event) => {
 			if (event.target.id === 'add-comment') {
 				handler(getElementDOM('#student-comment').value)();
 			}
@@ -40,9 +40,29 @@ class StudentView {
 			appendContentToParent(studentCard, studentElement(student));
 			appendContentToParent(
 				studentCard,
-				constructElement('button', 'add comment', { type: 'id', value: 'add-comment' })
+				constructElement(
+					'button',
+					'add comment',
+					{ type: 'id', value: 'add-comment' },
+					{
+						type: 'class',
+						value: 'border bg-blue-500 text-white p-2 rounded-full',
+					}
+				)
 			);
-			appendContentToParent(studentCard, constructElement('input', '', { type: 'id', value: 'student-comment' }));
+			appendContentToParent(
+				studentCard,
+				constructElement(
+					'input',
+					'',
+					{ type: 'id', value: 'student-comment' },
+					{
+						type: 'class',
+						value:
+							'w-1/6 border-b border-b-2 border-teal-500 py-2 appearance-none bg-transparent text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none',
+					}
+				)
+			);
 		}
 	}
 }
--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -31,13 +31,14 @@ export const studentElement = ({ id, first_name, last_name, DoB, image, studies,
 	const shouldDisplayComments =
 		comments && comments.length > 0 ? `<div>comments: ${comments.length}</div>` : '';
 	return `<li style="list-style:none" id="student_${id}" class="relative student m-4 w-1/4 flex-1 max-w-sm rounded overflow-hidden shadow-lg">
-			<img class="w-full" id="student-img_${id}" src="${image}" />
+			<img class="w-full student-img" id="student-img_${id}" src="${image}" />
 			<div class="px-6 py-4">
 				<div class="font-bold text-xl mb-2">First Name: ${first_name}</div>
 				<div class="font-bold text-xl mb-2">Last Name: ${last_name}</div>
 				<div class="font-bold text-xl mb-2">Date of Birth: ${DoB}</div>
 				<h3 class="font-bold text-xl mb-2">Studies</h3>
-				${removesCommasFromElementList(allStudies)}${shouldDisplayComments}
+				${removesCommasFromElementList(allStudies)}
+				${shouldDisplayComments}
 				<button id="delete-student" class="m-2 absolute top-0 right-0 bg-red-500 rounded-full px-3 py-1 text-sm font-semibold hover:bg-red-100 hover:text-black text-white mr-2" data-id="${id}">
 					delete
 				</button>
@@ -48,7 +49,10 @@ export const studentElement = ({ id, first_name, last_name, DoB, image, studies,
 export const studentComments = ({ comments }) => {
 	const commentsList =
 		comments.length > 0
-			? comments.map((comment, index) => `<div id="comment_${index}">${comment}</div>`)
+			? comments.map(
+					(comment, index) =>
+						`<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2" id="comment_${index}">${comment}</span>`
+			  )
 			: '';
 
 	return `<div class="students__comments">${removesCommasFromElementList(commentsList)}</div>`;
@@ -62,13 +66,15 @@ export const getUniqueAcademyPeriods = (studentsList) => {
 export const studentsSearchResult = (students) => {
 	const content = students.map(({ id, first_name, last_name, DoB }) =>
 		id
-			? `<div id="search-student-${id}">
-					<h3>Student Information</h3>
-					<div>First Name: ${first_name}</div>
-					<div>Last Name: ${last_name}</div>
-					<div>Date of Birth: ${DoB}</div>
+			? `<div id="search-student-${id}" class="m-4 max-w-sm rounded overflow-hidden shadow-lg">
+					<div class="px-6 py-4">
+						<div class="font-bold text-xl mb-2">${first_name} ${last_name}</div>
+						<div class="font-bold text-xl mb-2">Birthed on: ${DoB}</div>
+					</div>
 				</div>`
-			: `<div id="not-found">Student with this ID not found.</div>`
+			: `<div id="not-found" class="max-w-sm rounded overflow-hidden shadow-lg">
+				<p class="class="text-gray-700 text-base">Student with this ID not found.</p>
+			</div>`
 	);
 	return removesCommasFromElementList(content);
 };
