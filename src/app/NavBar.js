8e54faec924e3913751aa31afcb274296b51e56b
Michalis Koutridis <michalis.koutridis@camelotls.com>
2020-05-23 13:04:58 +0300

    improvement(styles): improves navbar ui

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -1,24 +1,32 @@
 import { getElementDOM } from '../utils/utilities';
 
+const ROUTES = {
+	home: '/',
+	students: '/students',
+};
+
+const DEFAULT_CLASS =
+	'nav-item bg-white inline-block py-2 px-4 text-blue-700 font-semibold focus:outline-none';
+const ACTIVE_CLASS = `-mb-px border-l border-t border-r rounded-t ${DEFAULT_CLASS}`;
 class NavBar {
 	constructor(routerInstance) {
 		this.routerInstance = routerInstance;
 		this.render();
 	}
 
-	bindNavigation() {
-		getElementDOM('body').addEventListener('click', (event) => {
-			const targetId = event.target.id;
+	setNavItemStyleToDefault() {
+		Array.from(document.querySelectorAll('.nav-item')).forEach((item) =>
+			item.setAttribute('class', DEFAULT_CLASS)
+		);
+	}
 
-			switch (targetId) {
-				case 'home':
-					this.routerInstance.navigate('/');
-					break;
-				case 'students':
-					this.routerInstance.navigate('/students');
-					break;
-				default:
-					break;
+	bindNavigation() {
+		getElementDOM('.navbar').addEventListener('click', (event) => {
+			const targetId = event.target.id;
+			if (ROUTES.hasOwnProperty(targetId)) {
+				this.setNavItemStyleToDefault();
+				this.routerInstance.navigate(ROUTES[targetId]);
+				document.getElementById(targetId).setAttribute('class', ACTIVE_CLASS);
 			}
 		});
 	}
@@ -26,12 +34,12 @@ class NavBar {
 	render() {
 		document.querySelector('body').insertAdjacentHTML(
 			'afterbegin',
-			`<ul class="flex border-b">
-				<li class="-mb-px mr-1">
-					<button id="home" class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold focus:outline-none">Home</button>
+			`<ul class="navbar flex border-b m-1">
+				<li class="mr-1">
+					<button id="home" class="${ACTIVE_CLASS}">Home</button>
 				</li>
 				<li class="mr-1">
-					<button id="students" class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold focus:outline-none">Students</button>
+					<button id="students" class="${DEFAULT_CLASS}">Students</button>
 				</li>
 			</ul>`
 		);
